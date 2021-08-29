11bdd7f691345221e839661c265acde1323f16c6
Rajjjjjjjj <48846157+Rajjjjjjjj@users.noreply.github.com>
2021-08-29 03:10:59 +0100

    index.js

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ const prefix = conJson.prefix;
 
 counterNum = 0;
 
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'))
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => console.log('Listening on port ' + port))
+
 bot.on('ready', () => {
     console.log('Online.');
 
